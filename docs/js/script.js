26f8763c9d09cb7fd8fe4486765f917b32d1a056
JLalomia <julianna@lalomia.com>
2024-10-22 09:27:19 -0400

    edited script.js

--- docs/js/script.js
+++ docs/js/script.js
@@ -8,13 +8,11 @@ document.addEventListener('DOMContentLoaded', function () {
         menuToggle.classList.toggle('open');
         menuItems.classList.toggle('active');
         
-        // Prevent scrolling when the menu is open
         document.body.style.overflow = isExpanded ? 'visible' : 'hidden';
     }
 
     menuToggle.addEventListener('click', toggleMenu);
 
-    // Close menu when clicking outside the menu
     document.addEventListener('click', function (event) {
         const isClickInside = menuItems.contains(event.target) || menuToggle.contains(event.target);
         if (!isClickInside && menuItems.classList.contains('active')) {
@@ -22,14 +20,12 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-    // Close menu when pressing Escape key
     document.addEventListener('keydown', function (event) {
         if (event.key === 'Escape' && menuItems.classList.contains('active')) {
             toggleMenu();
         }
     });
 
-    // Handle window resize
     window.addEventListener('resize', function () {
         if (window.innerWidth > 768 && menuItems.classList.contains('active')) {
             toggleMenu();
