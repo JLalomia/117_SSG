329ab2fffa9f538dfacf835322062bad4cb56d82
JLalomia <julianna@lalomia.com>
2024-12-17 10:30:59 -0500

    added more files to website

--- src/routes.js
+++ src/routes.js
@@ -10,9 +10,14 @@ const routes = {
     Nuclear: 'Nuclear.html',
     ARPA: 'ARPA.html',
     Arpanet: 'Arpanet.html',
+    CSS: 'CSS.html',
     LH: 'LinuxHis.html',
+    BrowserWars: 'BrowserWars.html',
+    BWars2: 'BWars2.html',
     ARIA: 'ARIA.html',
     AccMWD: 'AccMWD.html',
+    IOT: 'IOT.html',
+    QC: 'QuantumComp.html',
     notFound: '404.html'
 };
 
